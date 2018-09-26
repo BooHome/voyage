fc1824fae35ff15808a46ae88763be716cc12271
BooHome <fengshibo1024@gmail.com>
2018-09-26 10:52:20 +0800

    第一次提交测试

---
+++ src/main/java/com/ihere/voyage/VoyageApplication.java
@@ -0,0 +1,15 @@
+package com.ihere.voyage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author fengshibo
+ */
+@SpringBootApplication
+public class VoyageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VoyageApplication.class, args);
+    }
+}
---
+++ src/test/java/com/ihere/voyage/VoyageApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ihere.voyage;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VoyageApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
